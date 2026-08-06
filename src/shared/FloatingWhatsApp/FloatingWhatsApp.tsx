f0e1871416664dcaab9d6be5f2a187b36fc4ef45
Mohamed Hamdy <mohammed.hamdi.mohamed@gmail.com>
2026-08-06 20:51:59 +0300

    (fix) Floating button of whatsApp in mobile screen

--- src/shared/FloatingWhatsApp/FloatingWhatsApp.tsx
+++ src/shared/FloatingWhatsApp/FloatingWhatsApp.tsx
@@ -11,7 +11,7 @@ const FloatingWhatsApp = () => {
       rel="noopener noreferrer"
       aria-label="تواصل معنا عبر واتساب"
       title="تواصل معنا عبر واتساب"
-      className="fixed bottom-26 right-6 z-50 inline-flex items-center justify-center rounded-full bg-[#25D366] text-white shadow-lg transition-transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-[#25D366] focus:ring-offset-2"
+      className="fixed bottom-31 md:bottom-26 sm:bottom-10 right-7 z-50 inline-flex items-center justify-center rounded-full bg-[#25D366] text-white shadow-lg transition-transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-[#25D366] focus:ring-offset-2"
     >
       <span className="flex h-14 w-14 items-center justify-center sm:h-auto sm:w-auto sm:px-4 sm:py-3">
         <WhatsAppIcon className="h-6 w-6" />
